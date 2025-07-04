950da5a5881bfc18e25cf90176d414d17776a46b
Sadha Shiva Aeshala <shivaaeshala@gmail.com>
2025-07-05 01:44:27 +0530

    Changed BRouter to HRouter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
