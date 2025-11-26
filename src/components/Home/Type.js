3c5c1de411fb4c58dd9f3a5efb7c3135d935cca9
Sadha Shiva Aeshala <shivaaeshala@gmail.com>
2025-11-26 21:19:07 +0530

    Corrected spellings

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,8 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Full Stack Developer(MERN)",
           "Competitive Programmer",
+          "Full Stack Developer(MERN)",
           "Machine Learning Engineer",
           "Software Developer",
         ],
